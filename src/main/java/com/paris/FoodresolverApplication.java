cd3294eb980d0c7c27c9718f44a12f7f0ca9e385
boukhris <samyboukhris@gmail.com>
2016-11-11 19:41:40 +0100

    first commit

---
+++ src/main/java/com/paris/FoodresolverApplication.java
@@ -0,0 +1,12 @@
+package com.paris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodresolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodresolverApplication.class, args);
+	}
+}
